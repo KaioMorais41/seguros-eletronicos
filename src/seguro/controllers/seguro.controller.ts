a29ed6c7d362ed4cdb7fa371ff3011e52249d349
KaioMorais41 <kaiomorais2903@email.com>
2026-04-07 13:56:47 -0300

    feat: implementa endpoint PUT para atualizar registros

--- src/seguro/controllers/seguro.controller.ts
+++ src/seguro/controllers/seguro.controller.ts
@@ -1,42 +1,48 @@
 import { Seguro } from '../entities/seguro.entity';
 import { SeguroService } from './../services/seguro.service';
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 
 
 @Controller ("/seguros")
 export class SeguroController {
-    constructor(private readonly SeguroService: SeguroService){ }
+    constructor(private readonly seguroService: SeguroService){ }
 
 
     @Get()
     @HttpCode(HttpStatus.OK)
     findAll(): Promise<Seguro[]>{
-        return this.SeguroService.findAll();
+        return this.seguroService.findAll();
     }
 
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
     findById(@Param('id', ParseIntPipe)id:number){
-    return this.SeguroService.findById(id);
+    return this.seguroService.findById(id);
     }
 
 
     @Get('/cliente/:cliente')
     @HttpCode(HttpStatus.OK)
     findAllByCliente(@Param('cliente') cliente:string): Promise<Seguro[]>{
-    return this.SeguroService.findAllByCliente(cliente);
+    return this.seguroService.findAllByCliente(cliente);
     }
 
     @Post("")
     @HttpCode(HttpStatus.CREATED)
     create(@Body() seguro: Seguro): Promise<Seguro>{
-    return this.SeguroService.create(seguro);
+    return this.seguroService.create(seguro);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() seguro: Seguro): Promise<Seguro> {
+    return this.seguroService.update(seguro);
     }
 
     @Delete('/:id')
     @HttpCode(HttpStatus.NO_CONTENT)
     delete(@Param('id', ParseIntPipe)id:number){
-    return this.SeguroService.delete(id);
+    return this.seguroService.delete(id);
     }
     
 }
--- src/seguro/services/seguro.service.ts
+++ src/seguro/services/seguro.service.ts
@@ -1,59 +1,54 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Seguro } from '../entities/seguro.entity';
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { ILike, Repository } from 'typeorm';
-import { DeleteResult } from 'typeorm/browser';
-
+import { ILike, Repository, DeleteResult } from 'typeorm';
 
 @Injectable()
 export class SeguroService {
-    constructor (
-        @InjectRepository(Seguro) 
+
+    constructor(
+        @InjectRepository(Seguro)
         private seguroRepository: Repository<Seguro>
     ) { }
 
     async findAll(): Promise<Seguro[]> {
-        return await this.seguroRepository.find();
+        return this.seguroRepository.find();
     }
 
     async findById(id: number): Promise<Seguro> {
         const seguro = await this.seguroRepository.findOne({
             where: { id }
-     });
+        });
 
-     if (!seguro) {
-        throw new HttpException('Seguro não encontrado!', HttpStatus.NOT_FOUND);
-     }
+        if (!seguro) {
+            throw new HttpException('Seguro não encontrado!', HttpStatus.NOT_FOUND);
+        }
 
-     return seguro;
+        return seguro;
     }
 
-    async findAllByCliente(cliente: string): Promise<Seguro[]>{
-        return await this.seguroRepository.find({
-            where:{
+    async findAllByCliente(cliente: string): Promise<Seguro[]> {
+        return this.seguroRepository.find({
+            where: {
                 cliente: ILike(`%${cliente}%`)
             }
-        })
+        });
     }
 
-    async create (seguro: Seguro): Promise<Seguro> {
-        return await this.seguroRepository.save(seguro);
+    async create(seguro: Seguro): Promise<Seguro> {
+        return this.seguroRepository.save(seguro);
     }
 
-    async delete (id: number): Promise<DeleteResult>{
-        await this.findById(id)
+    async update(seguro: Seguro): Promise<Seguro> {
 
-        return await this.seguroRepository.delete(id)
-    }
-
+    await this.findById(seguro.id);
     
+    return await this.seguroRepository.save(seguro);
+}
 
-
-
+    async delete(id: number): Promise<DeleteResult> {
+        await this.findById(id);
+        return this.seguroRepository.delete(id);
     }
 
-
-
-
-
-
+}
